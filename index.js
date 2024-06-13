d18a91fa15925b60cdc110324e9effa4c6194716
maimuna-mohamed <maimunamohamedaden17@gmail.com>
2024-06-13 10:39:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+window.customerName = `bob`;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+// function  upperCaseCustomerName(){
+//     window.customerName = window.customerName.toupperCase() ;
+// }
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer){
+    window.bestCustomer = newBestCustomer;
+}
+
+// const leastFavoriteCustomer='initial value';
+
+// function changedLeastFavoriteCustomer(){
+//    leastFavoriteCustomer  = 'new value'; // This will throw an error because leastFavoriteCustomer is a constant
+//    // }
+// }
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';// This will throw an error because leastFavoriteCustomer is a constant
+}
